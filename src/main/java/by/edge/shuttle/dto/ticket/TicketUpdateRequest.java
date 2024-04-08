f280c806c960d9ba5110a466be837889c78657b8
Daniil Laman <edgesea@yandex.by>
2024-04-09 02:56:56 +0300

    Add files via upload

--- src/main/java/by/edge/shuttle/dto/ticket/TicketUpdateRequest.java
+++ src/main/java/by/edge/shuttle/dto/ticket/TicketUpdateRequest.java
@@ -1,7 +1,17 @@
 package by.edge.shuttle.dto.ticket;
 
+import by.edge.shuttle.entity.Passenger;
+import by.edge.shuttle.entity.Shuttle;
+import by.edge.shuttle.entity.Stop;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+
+import java.time.LocalTime;
 
 @Data
 public class TicketUpdateRequest {
+    @Pattern(regexp = "^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$")
+    private LocalTime departureTime;
+    @Pattern(regexp = "^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$")
+    private LocalTime arrivalTime;
 }
